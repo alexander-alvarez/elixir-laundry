e5b95f55b7d43484626783daa7274acaa9ff2d7f
Alex Alvarez <alexander-alvarez@github.com>
2017-02-13 23:33:53 -0500

    initial mix commit

---
+++ laundry/lib/laundry.ex
@@ -0,0 +1,18 @@
+defmodule Laundry do
+  @moduledoc """
+  Documentation for Laundry.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Laundry.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ laundry/test/laundry_test.exs
@@ -0,0 +1,8 @@
+defmodule LaundryTest do
+  use ExUnit.Case
+  doctest Laundry
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
